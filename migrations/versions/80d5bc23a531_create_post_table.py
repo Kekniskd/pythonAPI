3bf2067f096bbed6ad607d1afe3a4f9ce7b2f704
kedar <kekniskedar@gmail.com>
2022-01-09 15:32:59 +0530

    Added database migration tool alembic for better database management

---
+++ migrations/versions/80d5bc23a531_create_post_table.py
@@ -0,0 +1,26 @@
+"""Create post table
+
+Revision ID: 80d5bc23a531
+Revises: 
+Create Date: 2022-01-09 15:22:45.245413
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '80d5bc23a531'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('posts', sa.Column('id', sa.Integer(), nullable=False, primary_key=True), sa.Column('title', sa.String, nullable=False))
+    pass
+
+
+def downgrade():
+    op.drop_table('posts')
+    pass
---
+++ migrations/versions/c72772a55750_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c72772a55750
+Revises: 80d5bc23a531
+Create Date: 2022-01-09 15:31:10.046381
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c72772a55750'
+down_revision = '80d5bc23a531'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
