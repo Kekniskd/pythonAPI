34b55f9aab27dc1d715c974974b5d9540ed2e8f6
kedar <kekniskedar@gmail.com>
2022-01-09 11:27:53 +0530

    Done with all changes and cleaning creating config file for envirmental veriables

--- app/main.py
+++ app/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from app.database import engine
 from . import models
 from .routers import post, user, auth
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_password: str = 'localhost'
+    database_username: str = 'postgres'
+    secret_key: str = "afda3545321543"
+
+settings = Settings()
 
 models.Base.metadata.create_all(bind=engine)
 
