f6f47a68a4798212681efc393a2b98ee56f6d07c
kedar <kekniskedar@gmail.com>
2022-01-10 18:29:58 +0530

    removed bug from create user url where it was givving internal server error when you trying to create user account from existing user email address

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import status, HTTPException, Depends, APIRouter
+from fastapi import status, HTTPException, Depends, APIRouter, status
 from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -13,13 +13,17 @@ router = APIRouter(
 # Create user
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def createUser(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    hashedPass = utils.hash(user.password)
-    user.password = hashedPass
-    newUser = models.User(**user.dict())
-    db.add(newUser)
-    db.commit()
-    db.refresh(newUser)
-    return newUser
+    present_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if present_user:
+        raise HTTPException(status_code=status.HTTP_303_SEE_OTHER, detail=f"{user.email} already exists!!")
+    else:
+        hashedPass = utils.hash(user.password)
+        user.password = hashedPass
+        newUser = models.User(**user.dict())
+        db.add(newUser)
+        db.commit()
+        db.refresh(newUser)
+        return newUser
 
 
 # Get user by ID
